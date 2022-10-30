5be13190d3f1e9cce5608cb22155858ef80d16f5
Puyodead1 <puyodead@protonmail.com>
2022-10-30 15:04:00 -0400

    some more error work

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,11 +1,13 @@
 package userscontroller
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 
 	"github.com/Puyodead1/fosscord-server-go/models"
 	userservices "github.com/Puyodead1/fosscord-server-go/services"
-	"github.com/Puyodead1/fosscord-server-go/utils/errors"
+	fcerrors "github.com/Puyodead1/fosscord-server-go/utils/errors"
 	"github.com/Puyodead1/fosscord-server-go/utils/errors/fielderror"
 	"github.com/Puyodead1/fosscord-server-go/utils/errors/httperror"
 	"github.com/Puyodead1/fosscord-server-go/utils/errors/jsonerrors"
@@ -14,15 +16,16 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// TODO: how do we validate boolean values? using binding="boolean" results in validation errors as if they weren't even provided
 type RegisterRequest struct {
 	CaptchaKey            string `json:"captcha_key" binding:"ascii"`
-	Consent               bool   `json:"consent" binding:"boolean"`
-	DOB                   string `json:"dob" binding:"required,ascii"`
+	Consent               bool   `json:"consent"`
+	DOB                   string `json:"date_of_birth" binding:"required,ascii"`
 	Email                 string `json:"email" binding:"required,email"`
 	GiftCodeSkuId         string `json:"gift_code_sku_id" binding:"ascii"`
 	Invite                string `json:"invite" binding:"ascii"`
-	Password              string `json:"password" binding:"required,printascii,min=8"` // TODO: password policy
-	PromotionalEmailOptIn bool   `json:"promotional_email_opt_in" binding:"boolean"`
+	Password              string `json:"password" binding:"required,printascii,min=6,max=72"` // TODO: password policy
+	PromotionalEmailOptIn bool   `json:"promotional_email_opt_in"`
 	Username              string `json:"username" binding:"required,ascii,min=2,max=32"`
 }
 
@@ -39,35 +42,51 @@ func Register(c *gin.Context) {
 	// validate the request body is RegisterRequest
 	var req RegisterRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		e := errors.HTTPError{
-			Code:    int(jsonerrors.InvalidFormBody),
-			Message: jsonerrors.JSONErrorMessages[jsonerrors.InvalidFormBody],
-		}
-		errs := err.(validator.ValidationErrors)
-		for _, err := range errs {
-			e.Errors = &map[string]errors.FieldError{
-				err.Field(): {
-					EErrors: []errors.FieldErrorErrors{
-						{
-							Code:    err.Tag(),
-							Message: err.Error(),
+		var serr *json.SyntaxError
+		var verr validator.ValidationErrors
+		if errors.As(err, &verr) {
+			e := fcerrors.HTTPError{
+				Code:    int(jsonerrors.InvalidFormBody),
+				Message: jsonerrors.JSONErrorMessages[jsonerrors.InvalidFormBody],
+			}
+			errs := err.(validator.ValidationErrors)
+			for _, err := range errs {
+				e.Errors = &map[string]fcerrors.FieldError{
+					err.Field(): {
+						EErrors: []fcerrors.FieldErrorErrors{
+							{
+								Code:    fielderror.ValidationErrors[err.Tag()].String(),
+								Message: fielderror.ValidationErrors[err.Tag()].Message(),
+							},
 						},
 					},
-				},
+				}
 			}
+			c.JSON(400, e)
+			return
+		} else if errors.As(err, &serr) {
+			c.JSON(400, fcerrors.HTTPError{
+				Code:    int(jsonerrors.InvalidJSON),
+				Message: jsonerrors.JSONErrorMessages[jsonerrors.InvalidJSON],
+			})
+			return
+		} else {
+			c.JSON(400, fcerrors.HTTPError{
+				Code:    int(jsonerrors.GeneralError),
+				Message: jsonerrors.JSONErrorMessages[jsonerrors.GeneralError],
+			})
+			return
 		}
-		c.JSON(400, e)
-		return
 	}
 
 	// check if the email already exists
 	if userservices.GetUserByEmail(req.Email).ID != "" {
-		c.JSON(400, errors.HTTPError{
+		c.JSON(400, fcerrors.HTTPError{
 			Code:    int(jsonerrors.InvalidFormBody),
 			Message: jsonerrors.JSONErrorMessages[jsonerrors.InvalidFormBody],
-			Errors: &map[string]errors.FieldError{
+			Errors: &map[string]fcerrors.FieldError{
 				"email": {
-					EErrors: []errors.FieldErrorErrors{
+					EErrors: []fcerrors.FieldErrorErrors{
 						{
 							Code:    fielderror.EMAIL_ALREADY_REGISTERED.String(),
 							Message: fielderror.EMAIL_ALREADY_REGISTERED.Message(),
@@ -82,7 +101,7 @@ func Register(c *gin.Context) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
 	if err != nil {
 		log.Fatalf("[Registration] Failed to hash password: %v", err)
-		c.JSON(500, errors.HTTPError{Code: 500, Message: string(httperror.InternalServerError)})
+		c.JSON(500, fcerrors.HTTPError{Code: 500, Message: string(httperror.InternalServerError)})
 		return
 	}
 
@@ -98,7 +117,7 @@ func Register(c *gin.Context) {
 	// create the user in the database
 	if err := userservices.CreateUser(&user); err != nil {
 		log.Fatalf("[Registration] Failed to create user: %v", err)
-		c.JSON(500, errors.HTTPError{Code: 500, Message: string(httperror.InternalServerError)})
+		c.JSON(500, fcerrors.HTTPError{Code: 500, Message: string(httperror.InternalServerError)})
 		return
 	}
 
@@ -106,7 +125,7 @@ func Register(c *gin.Context) {
 	token, err := userservices.GenerateToken(user.ID)
 	if err != nil {
 		log.Fatalf("[Registration] Failed to generate user: %v", err)
-		c.JSON(500, errors.HTTPError{Code: 500, Message: string(httperror.InternalServerError)})
+		c.JSON(500, fcerrors.HTTPError{Code: 500, Message: string(httperror.InternalServerError)})
 		return
 	}
 
--- utils/errors/fielderror/fielderrors.go
+++ utils/errors/fielderror/fielderrors.go
@@ -6,13 +6,23 @@ const (
 	EMAIL_TYPE_INVALID_EMAIL FieldErrorCode = "EMAIL_TYPE_INVALID_EMAIL"
 	EMAIL_ALREADY_REGISTERED FieldErrorCode = "EMAIL_ALREADY_REGISTERED"
 	BASE_TYPE_MIN_LENGTH     FieldErrorCode = "BASE_TYPE_MIN_LENGTH"
+	BASE_TYPE_MAX_LENGTH     FieldErrorCode = "BASE_TYPE_MAX_LENGTH"
 	BASE_TYPE_REQUIRED       FieldErrorCode = "BASE_TYPE_REQUIRED"
 )
 
+var ValidationErrors = map[string]FieldErrorCode{
+	"email":    EMAIL_TYPE_INVALID_EMAIL,
+	"min":      BASE_TYPE_MIN_LENGTH,
+	"max":      BASE_TYPE_MAX_LENGTH,
+	"required": BASE_TYPE_REQUIRED,
+}
+
+// TODO: min and max should be configurable and able to be different for each field
 var FieldErrorMessages = map[FieldErrorCode]string{
 	EMAIL_TYPE_INVALID_EMAIL: "Not a well formed email address.",
 	EMAIL_ALREADY_REGISTERED: "Email is already registered.",
 	BASE_TYPE_MIN_LENGTH:     "Must be 6 or more in length.",
+	BASE_TYPE_MAX_LENGTH:     "Must be 72 or fewer in length.",
 	BASE_TYPE_REQUIRED:       "This field is required",
 }
 
